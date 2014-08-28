171a5ee74cec08f6933cc404b288f1e885a6d77b
silveryiris <silveryiris@gmail.com>
2014-08-28 15:30:57 +0800

    plugin update 2

--- code-phoenix.php
+++ code-phoenix.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8"> 
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400italic,700italic,400,700|Oswald:400,700" rel="stylesheet">
 		<link href="lib/bootstrap-select.min.css" rel="stylesheet">
 		<link href="lib/jquery.bootgrid.css" rel="stylesheet">
